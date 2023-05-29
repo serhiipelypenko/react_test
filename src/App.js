d14a3ba8ce4b7893963dc8a5b2864f32f8c02ef4
serhiipelypenko <sergey.pelipenko@triogeeks.com>
2023-05-29 20:46:38 +0300

    Increment Decrement

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 function App() {
+    const [likes, setLikes] = useState(0);
+
+    function increment() {
+        setLikes(likes + 1);
+    }
+    function decrement() {
+        setLikes(likes - 1);
+    }
+
   return (
     <div className="App">
-        Test
+        <h1>{likes}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
